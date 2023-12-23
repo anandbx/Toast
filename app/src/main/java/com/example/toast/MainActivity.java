494c00bba51dd4034625c4c1ed003335fe148de5
Admin <anandxbr@gmail.com>
2023-12-23 09:35:12 +0530

    Initial commit

---
+++ app/src/main/java/com/example/toast/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnToast;
+
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnToast = findViewById(R.id.btnToast);
+
+        btnToast.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"This is my First Toast",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+}
